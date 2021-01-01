96344b1983c28061b16ca836e8345b74f94f6ae4
NoemiHeusler <58665516+noemiheusler@users.noreply.github.com>
2021-01-01 14:45:44 +0100

    Update sbb.js

--- sbb.js
+++ sbb.js
@@ -1,3 +1,21 @@
+
+function goFullscreen() {
+  // Must be called as a result of user interaction to work
+  mf = document.getElementById("main_frame");
+  mf.webkitRequestFullscreen();
+  mf.style.display="";
+}
+function fullscreenChanged() {
+  if (document.webkitFullscreenElement == null) {
+      mf = document.getElementById("main_frame");
+      mf.style.display="none";
+  }
+}
+
+document.onwebkitfullscreenchange = fullscreenChanged;
+document.documentElement.onclick = goFullscreen;
+document.onkeydown = goFullscreen;
+
 const departureHome = () => {
   const url = "https://transport.opendata.ch/v1/stationboard?station=Wallisellen,Neugut&limit=10"
   
@@ -31,8 +49,12 @@ const departureHome = () => {
         ziel = station.to
         abfahrt = new Date(station.stop.departure)
         abfahrt.toISOString().substring(0, 10)
-        bisAbfahrt = ((abfahrt.getTime() - today.getTime())/(1000*60)).toFixed(2);
-        if (bisAbfahrt < 6) {
+
+        bisAbfahrt = ((abfahrt.getTime() - today.getTime())/(1000*60)).toFixed();
+        if (bisAbfahrt < 0) {
+          row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">zu spät</td></tr`
+          tbody.insertAdjacentHTML("afterbegin", row)
+        } else if (bisAbfahrt < 6) {
           row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">${bisAbfahrt} min</td></tr`
           tbody.insertAdjacentHTML("afterbegin", row)
         } else {
@@ -48,4 +70,4 @@ const departureHome = () => {
     });
 };
 
-window.setInterval(departureHome, 10000);
+window.setInterval(departureHome, 30000);
