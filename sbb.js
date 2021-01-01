bd9a8ea41cb4eac5aad805ebbe9bc709f6987677
Sascha Gysel <58490316+saschagysel@users.noreply.github.com>
2021-01-01 14:30:49 +0100

    Update sbb.js

--- sbb.js
+++ sbb.js
@@ -10,22 +10,31 @@ const departureHome = () => {
   let minutes = today.getMinutes();
 
   const h2 = document.querySelector("h2");
-  h2.insertAdjacentHTML("afterbegin", `${hours}:${minutes}`)
+  h2.innerHTML = '';
+
+
+  if (minutes < 10) {
+    h2.insertAdjacentHTML("afterbegin", `${hours}:0${minutes}`)
+  } else {
+    h2.insertAdjacentHTML("afterbegin", `${hours}:${minutes}`)
+  }
+
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = '';
 
   fetch(url)
     .then(response => response.json())
     .then((data) => {
       const stationboard = data.stationboard.reverse();
-      const tbody = document.querySelector("tbody");
 
       stationboard.forEach((station) => {
         ziel = station.to
         abfahrt = new Date(station.stop.departure)
         abfahrt.toISOString().substring(0, 10)
-        bisAbfahrt = ((abfahrt.getTime() - today.getTime())/(1000*60)).toFixed(0);
+        bisAbfahrt = ((abfahrt.getTime() - today.getTime())/(1000*60)).toFixed(2);
         if (bisAbfahrt < 6) {
-        row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">${bisAbfahrt} min</td></tr`
-        tbody.insertAdjacentHTML("afterbegin", row)
+          row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">${bisAbfahrt} min</td></tr`
+          tbody.insertAdjacentHTML("afterbegin", row)
         } else {
           row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td>${bisAbfahrt} min</td></tr`
           tbody.insertAdjacentHTML("afterbegin", row)
@@ -39,10 +48,4 @@ const departureHome = () => {
     });
 };
 
-departureHome();
-
-const lager = () => {
-      const tbody = document.querySelector("tbody")
-      const row = '<tr><th scope="row"></th><td>Linie 12</td><td>Ziel</td><td>Abfahrt in</td></tr>'
-      tbody.insertAdjacentHTML("afterbegin", row);
-} 
+window.setInterval(departureHome, 10000);
