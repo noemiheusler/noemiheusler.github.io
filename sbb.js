1c0607206bc4bc56bcab2d72301edd75c0df5b1f
NoemiHeusler <58665516+noemiheusler@users.noreply.github.com>
2021-01-01 21:01:58 +0100

    Update sbb.js

--- sbb.js
+++ sbb.js
@@ -1,5 +1,6 @@
+// function for fullscreen kiosk mode
+
 function goFullscreen() {
-  // Must be called as a result of user interaction to work
   mf = document.getElementById("main_frame");
   mf.webkitRequestFullscreen();
   mf.style.display="";
@@ -16,6 +17,8 @@ document.onwebkitfullscreenchange = fullscreenChanged;
 document.documentElement.onclick = goFullscreen;
 document.onkeydown = goFullscreen;
 
+// function to find out iOS version for our stupid old tablet
+
 function iOSversion() {
   if (/iP(hone|od|ad)/.test(navigator.platform)) {
     // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
@@ -26,18 +29,17 @@ function iOSversion() {
 
 ver = iOSversion()
 
+// function to calculate shown time and destination
+
 const departureHome = () => {
   const url = "https://transport.opendata.ch/v1/stationboard?station=Wallisellen,Neugut&limit=10"
   
   let today = new Date().getTime();
-  console.log(today);
-
   let hours = new Date().getHours();
   let minutes = new Date().getMinutes();
 
   const h2 = document.querySelector("h2");
   h2.innerHTML = '';
-
 
   if (minutes < 10) {
     h2.insertAdjacentHTML("afterbegin", `${hours}:0${minutes}`)
@@ -58,18 +60,18 @@ const departureHome = () => {
         departure_raw = station.stop.departure
         departure = departure_raw.slice(0, -5);
         console.log(departure);
-        //abfahrt = new Date(station.stop.departure).getTime();
+
+        // making sure of ios version
         if (ver == `12,5,0`) {
           abfahrt = Date.parse(departure);
-          //console.log(abfahrt);
           bisAbfahrt = ((abfahrt-today)/(1000*60)).toFixed();
           bisAbfahrt = bisAbfahrt-60;
         } else {
           abfahrt = Date.parse(departure);
-          //console.log(abfahrt);
           bisAbfahrt = ((abfahrt-today)/(1000*60)).toFixed();
         }
         
+        // diverent optics of time
         if (bisAbfahrt <= 0) {
           row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">Zu spät</td></tr`
           tbody.insertAdjacentHTML("afterbegin", row)
@@ -81,14 +83,9 @@ const departureHome = () => {
           tbody.insertAdjacentHTML("afterbegin", row)
         }
       });
-
-      const times = document.querySelectorAll(".time");
-      times.forEach((time) =>{
-        time.classList.add("bold-font");
-      });
     });
 };
 
-
 departureHome();
-//window.setInterval(departureHome, 30000);
+
+window.setInterval(departureHome, 30000);
